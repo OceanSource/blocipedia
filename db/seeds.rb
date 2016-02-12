919899e30250e9a88d781bf794bdd3ae59850848
Ocean Source <oceansource@gmail.com>
2016-02-12 23:07:46 +0000

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,8 @@
   require 'faker'
   
- # Create Wikis and users
-
- 50.times do
-   Wiki.create!(
-     user: users.sample,
-     title: Faker::Lorem.sentence,
-     body:  Faker::Lorem.paragraph
-   )
- end
-  
-wikis = Wiki.all
- 
 #Create users
-5.times do
-  user = User.new(
-    name: Faker::Name.name,
+1.times do
+    user = User.new(
     email: Faker::Internet.email,
     password: Faker::Lorem.characters(10)
   )
@@ -26,15 +13,27 @@ end
  
  #Create admin user
  ocean = User.new(
-    name: 'Ocean Source',
-    email: 'oceansource@gmail.com',
+    email: 'oceansourced@gmail.com',
     password: 'passpass',
-    role: 'admin'
+    role: 2
   )
   ocean.skip_confirmation!
   ocean.save
   
   users = User.all
+ 
+ 
+ 50.times do
+   Wiki.create!(
+     user: users.sample,
+     title: Faker::Lorem.sentence,
+     body:  Faker::Lorem.paragraph
+   )
+ end
+  
+wikis = Wiki.all
+ 
+ 
  
  puts "Seed finished"
  puts "#{Wiki.count} wikis created"
