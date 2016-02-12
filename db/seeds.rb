66e79d089cd7f8de539197e6f371817c0ffdd339
Ocean Source <oceansource@gmail.com>
2016-02-12 22:41:57 +0000

    Added seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,17 @@
   
 wikis = Wiki.all
  
- #Create users
- 5.times do
-    user = User.new(
-      name: Faker::Name.name,
-      email: Faker::Internet.email,
-      password: Faker::Lorem.characters(10)
-    )
+#Create users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
     
-    user.skip_confirmation!
-    user.save
-    
- end
+  user.skip_confirmation!
+  user.save
+end
  
  #Create admin user
  ocean = User.new(
